f1a995f3b82ce768e8c60fe3d00c38ba63ea4454
Srikanth Badugu <badugulaxmisrikanth@gmail.com>
2025-11-20 05:18:44 +0000

    Remove invalid product-trend-tracker folder

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,5 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from product_trend_tracker.recommender_api import app
 from fastapi.testclient import TestClient
+from product_trend_tracker.recommender_api import app
 
 client = TestClient(app)
 
