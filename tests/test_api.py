a0e5c900807cbb6d2e7db5e904da4ff37e3dfb7a
Srikanth Badugu <badugulaxmisrikanth@gmail.com>
2025-11-20 05:37:08 +0000

    Rename Online_eval to test_pipeline_basic and clean tests

--- tests/test_api.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from product_trend_tracker.recommender_api import app
-
-client = TestClient(app)
-
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
